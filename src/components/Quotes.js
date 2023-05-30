a3e6254ad098a92a9ce32b7505b27ac2121da65f
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-30 11:44:35 +0200

    Fetch and display quotes

---
+++ src/components/Quotes.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, React } from 'react';
+
+function Quotes() {
+  const link = 'https://api.api-ninjas.com/v1/quotes?category=computers';
+
+  const [quotes, setQuotes] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(link, {
+        headers: { 'X-Api-Key': 'VwbFJrA2fRyoAlxo2UemCw==6Fg3cU4buiz80nwI' },
+      });
+      const json = await res.json();
+      setQuotes(json);
+      setIsPending(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {isPending && <h1>Loading...</h1>}
+      {quotes && quotes.map((quotes) => (
+        <li key={quotes.author}>
+          <h2>{quotes.author}</h2>
+          <p>{quotes.quote}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Quotes;
