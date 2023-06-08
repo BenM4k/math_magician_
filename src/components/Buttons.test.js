49fd428947df8a0191aa7ab1b18ca71b27267b8b
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-06-08 13:52:34 +0200

    Update Buttons.test.js

--- src/components/Buttons.test.js
+++ src/components/Buttons.test.js
@@ -7,7 +7,8 @@ import Button from './Buttons';
 describe('testing my Button component', () => {
   // Tests that the Button component renders without the computeFunction prop.
   test('test_button_computeFunction_not_passed', () => {
-    const { container } = render(<Button name="Test Button" />);
+    const computeFunction = jest.fn();
+    const { container } = render(<Button name="Test Button" computeFunction={computeFunction}/>);
 
     const element = container.firstChild;
 
